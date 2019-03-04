b72a3a36a0d81c35cc5399a73520015349ca33cf
맹주영 <mjy9088@naver.com>
2019-03-04 18:55:11 +0900

    (임시)

---
+++ codeup.kr/1369.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	int N, K, i, j;
+	scanf("%d %d", &N, &K);
+	for(int i = 0; i < N; i++)
+	{
+		printf("*");
+	}
+	printf("**\n");
+	return 0;
+}
+
