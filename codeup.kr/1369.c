18e3cd3d7cbf58010e471c1e59630f2c22751283
맹주영 <mjy9088@naver.com>
2019-03-05 15:03:14 +0900

    제출

--- codeup.kr/1369.c
+++ codeup.kr/1369.c
@@ -3,12 +3,35 @@
 int main(int argc, char **argv)
 {
 	int N, K, i, j;
+	char *test = "                                                                                                  *" + 98;
 	scanf("%d %d", &N, &K);
-	for(int i = 0; i < N; i++)
+	if(N == 1)
+	{
+		printf("*");
+		return 0;
+	}
+	N -= 2;
+	for(i = 0; i < N; i++)
 	{
 		printf("*");
 	}
-	printf("**\n");
+	printf("**\n*");
+	for(i = 0; i < N; i++)
+	{
+		for(j = 0; j <= N; j++)
+		{
+			int tmp = K - (i + j + K - 1) % K - 1;
+			j += tmp;
+			if(j > N) tmp -= j - N;
+			printf("%s", test - tmp);
+		}
+		printf("\n*");
+	}
+	for(i = 0; i < N; i++)
+	{
+		printf("*");
+	}
+	printf("*");
 	return 0;
 }
 
