9c4ca5c522f8b9086e7199d09d7a84c5c5aff341
맹주영 <mjy9088@naver.com>
2019-03-12 22:37:09 +0900

    더 간결하게

--- codeup.kr/1267/main.py
+++ codeup.kr/1267/main.py
@@ -1,4 +1,3 @@
 from functools import reduce
 
-int(input())
-print(reduce((lambda result, tmp: int(tmp % 5 == 0) * tmp + result), map(int, input().split()), 0))
+input(), print(reduce((lambda result, tmp: int(tmp % 5 == 0) * tmp + result), map(int, input().split()), 0))
