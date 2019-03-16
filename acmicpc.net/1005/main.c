469fb7267e8bd2ddb132ca9abad014c223d4c2b5
맹주영 <mjy9088@naver.com>
2019-03-17 03:04:40 +0900

    임시 저장

--- acmicpc.net/1005/main.c
+++ acmicpc.net/1005/main.c
@@ -10,7 +10,24 @@ typedef struct _tagB
 {
 	R *head;
 	int time;
+	int cal;
 } B;
+
+int solve(B *arr, int t)
+{
+	int result = arr[t].time;
+	R *tmp = arr[t].head;
+	printf(" %d", t);
+	while(tmp)
+	{
+		if(arr[tmp->b].cal == -1)
+		{
+			result += solve(arr, tmp->b);
+		}
+		tmp = tmp->next;
+	}
+	return arr[t].cal = result;
+}
 
 int main(int argc, char **argv)
 {
@@ -23,8 +40,9 @@ int main(int argc, char **argv)
 		R tmp[K];
 		for(j = 0; j < N; j++)
 		{
-			arr[j].head = NULL;
 			scanf("%d", &arr[j].time);
+			arr[j].head = NULL;
+			arr[j].cal = -1;
 		}
 		for(j = 0; j < K; j++)
 		{
@@ -32,6 +50,8 @@ int main(int argc, char **argv)
 			tmp[j].next = arr[t].head;
 			arr[t].head = &tmp[j];
 		}
+		scanf("%d", &t);
+		printf("%d\n", solve(arr, t));
 	}
 	return 0;
 }
