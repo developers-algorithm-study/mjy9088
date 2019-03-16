a48635eb070d67b03a0ccec92289e2ca11826bae
맹주영 <mjy9088@naver.com>
2019-03-17 02:21:24 +0900

    아니 이게 왜 안 되지?

---
+++ codeup.kr/1506/main.py
@@ -0,0 +1,30 @@
+N = int(input())
+arr = [[None] * N] * N
+s = N
+c, x, y = 0, 0, 0
+while s:
+	c += 1;
+	arr[y][x] = c
+	if s == 1:
+		break
+	for i in range(1, s):
+		y += 1
+		c += 1
+		arr[y][x] = c
+	for i in range(1, s):
+		x += 1
+		c += 1
+		arr[y][x] = c
+	for i in range(1, s):
+		y -= 1
+		c += 1
+		arr[y][x] = c
+	for i in range(2, s):
+		x -= 1
+		c += 1
+		arr[y][x] = c
+	y += 1
+	s -= 2
+
+for i in range(N):
+	print(' '.join(map(str, arr[i])))
