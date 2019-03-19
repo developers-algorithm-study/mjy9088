46de1f556e4f8dc8c209da3419f18c0400160410
맹주영 <mjy9088@naver.com>
2019-03-20 01:32:05 +0900

    버그 수정, C++ 풀이 추가

--- codeup.kr/2018/main.cpp
+++ codeup.kr/2018/main.cpp
@@ -1,31 +1,38 @@
-#include <stdio.h>
+#include <iostream>
 
-typedef struct
+struct i2
 {
 	int n, c;
-} i2;
+};
+
+template<typename T>
+void swap(T &a, T &b)
+{
+	T tmp = a;
+	a = b;
+	b = tmp;
+}
 
 int main(int argc, char **argv)
 {
 	int S, E, i, j, len, idx;
-	scanf("%d %d", &S, &E);
+	std::cin >> S;
+	std::cin >> E;
 	if(S == 1)
 	{
-		printf("1 \n");
+		std::cout << "1 " << std::endl;
 	}
 	S -= 2;
 	if(E == 1)
 	{
 		return 0;
 	}
-	i2 A[452], B[591], *a = A, *b = B, *temp;
+	i2 A[452], B[591], *a = A, *b = B;
 	len = 1;
 	a[0] = (i2){1, 1};
 	for(i = 1; i < E; i++)
 	{
-		temp = a;
-		a = b;
-		b = temp;
+		swap<i2 *>(a, b);
 		a[idx = 0] = (i2){b[0].n, 1};
 		if(a[idx].n == b[0].c)
 		{
@@ -37,13 +44,13 @@ int main(int argc, char **argv)
 		}
 		if(S < i)
 		{
-			printf("%d %d ", b[0].n, b[0].c);
+			std::cout << b[0].n << " " << b[0].c << " ";
 		}
 		for(j = 1; j < len; j++)
 		{
 			if(S < i)
 			{
-				printf("%d %d ", b[j].n, b[j].c);
+				std::cout << b[j].n << " " << b[j].c << " ";
 			}
 			if(a[idx].n == b[j].n)
 			{
@@ -64,7 +71,7 @@ int main(int argc, char **argv)
 		}
 		if(S < i)
 		{
-			printf("\n");
+			std::cout << std::endl;
 		}
 		len = idx + 1;
 	}
