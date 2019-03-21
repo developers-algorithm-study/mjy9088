3715bd9a4b3d81beae9adcf90d6a292126c13cea
맹주영 <mjy9088@naver.com>
2019-03-22 01:03:10 +0900

    잘못 저장한 거 되돌림

--- acmicpc.net/1010/main.c
+++ acmicpc.net/1010/main.c
@@ -1,29 +1,42 @@
 #include <stdio.h>
 
-int solve(int a, int b)
+long solve(int s, int b)
 {
-	if(b == 1)
+	long result = 1, i;
+	int tmp, p2 = 0, p3 = 0, p5 = 0, p7 = 0;
+	for(i = 0; i < s; i++)
 	{
-		return a % 10;
+		tmp = b - i;
+		while(tmp % 2 == 0) { p2++; tmp /= 2; }
+		while(tmp % 3 == 0) { p3++; tmp /= 3; }
+		while(tmp % 5 == 0) { p5++; tmp /= 5; }
+		while(tmp % 7 == 0) { p7++; tmp /= 7; }
+		result *= tmp;
 	}
-	if(b % 2)
+	for(i = 0; i < s; i++)
 	{
-		return a * solve((a * a) % 10, b / 2) % 10;
+		tmp = i + 1;
+		while(p2 && tmp % 2 == 0) { p2--; tmp /= 2; }
+		while(p3 && tmp % 3 == 0) { p3--; tmp /= 3; }
+		while(p5 && tmp % 5 == 0) { p5--; tmp /= 5; }
+		while(p7 && tmp % 7 == 0) { p7--; tmp /= 7; }
+		result /= tmp;
 	}
-	else
-	{
-		return b ? solve((a * a) % 10, b / 2) % 10 : 1;
-	}
+	while(p2) { p2--; result *= 2; }
+	while(p3) { p3--; result *= 3; }
+	while(p5) { p5--; result *= 5; }
+	while(p7) { p7--; result *= 7; }
+	return result;
 }
 
 int main(int argc, char **argv)
 {
-	int T, A, B, i;
+	int T, N, M;
 	scanf("%d", &T);
-	for(i = 0; i < T; i++)
+	for(int i = 0; i < T; i++)
 	{
-		scanf("%d %d", &A, &B);
-		printf("%d\n", (9 + solve(A, B)) % 10 + 1);
+		scanf("%d %d", &N, &M);
+		printf("%d\n", solve(N, M));
 	}
 	return 0;
 }
