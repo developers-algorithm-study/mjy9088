cb1b85d50caf62553254d0c2e049627c670d1903
맹주영 <mjy9088@naver.com>
2019-03-29 00:02:25 +0900

    Java 풀이 추가

---
+++ codeup.kr/1097/Main.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String... args)
+	{
+		Scanner sc = new Scanner(System.in);
+		int map[][] = new int[19][19];
+		for(int i = 0; i < 19; i++)
+		{
+			for(int j = 0; j < 19; j++)
+			{
+				map[i][j] = sc.nextInt();
+			}
+		}
+		int N = sc.nextInt();
+		for(int i = 0; i < N; i++)
+		{
+			int x = sc.nextInt() - 1, y = sc.nextInt() - 1;
+			for(int j = 0; j < 19; j++)
+			{
+				map[y][j] = map[y][j] == 1 ? 0 : 1;
+				map[j][x] = map[j][x] == 1 ? 0 : 1;
+			}
+		}
+		for(int i = 0; i < 19; i++)
+		{
+			for(int j = 0; j < 19; j++)
+			{
+				System.out.print(map[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+}
