7ffa0eb8f47532854279da24a6d71f828cd14a4d
맹주영 <mjy9088@naver.com>
2019-03-28 23:54:31 +0900

    C++ 풀이 추가

---
+++ codeup.kr/1097/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	int map[19][19], N, x, y;
+	for(int i = 0; i < 19; i++)
+	{
+		for(int j = 0; j < 19; j++)
+		{
+			std::cin >> map[i][j];
+		}
+	}
+	std::cin >> N;
+	for(int i = 0; i < N; i++)
+	{
+		std::cin >> x;
+		std::cin >> y;
+		x -= 1;
+		y -= 1;
+		for(int j = 0; j < 19; j++)
+		{
+			map[y][j] = !map[y][j];
+			map[j][x] = !map[j][x];
+		}
+	}
+	for(int i = 0; i < 19; i++)
+	{
+		for(int j = 0; j < 19; j++)
+		{
+			std::cout << map[i][j] << " ";
+		}
+		std::cout << std::endl;
+	}
+	return 0;
+}
+
