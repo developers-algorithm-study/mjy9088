b6391489ba22c9a69f1e3cda9ef7d47c5f6b1b5c
맹주영 <mjy9088@naver.com>
2019-04-03 01:57:43 +0900

    풀이 추가. 나중에 개선해야지

---
+++ acmicpc.net/2193/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdint.h>
+
+int main(int argc, char **argv)
+{
+	int N, i;
+	uint64_t X[2] = {0, 1}, Y[2], *x = X, *y = Y, *z;
+	scanf("%d", &N);
+	for(i = 1; i < N; i++)
+	{
+		z = x;
+		x = y;
+		y = z;
+		x[1] = y[0];
+		x[0] = y[0] + y[1];
+	}
+	printf("%ld\n", x[0] + x[1]);
+	return 0;
+}
+
