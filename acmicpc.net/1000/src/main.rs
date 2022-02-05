9476510304551216957acf196ca94d6696bb32c2
맹주영 <mjy9088@naver.com>
2022-02-05 17:38:00 +0900

    change assert to debug_assert

--- acmicpc.net/1000/src/main.rs
+++ acmicpc.net/1000/src/main.rs
@@ -2,13 +2,14 @@ use std::io;
 
 fn main() {
 	let mut line = String::new();
-	assert!(!io::stdin().read_line(&mut line).is_err());
+	let err = io::stdin().read_line(&mut line);
+	debug_assert!(!err.is_err());
 
 	let numbers = line
 		.trim()
 		.split(' ')
 		.flat_map(str::parse::<i32>)
 		.collect::<Vec<_>>();
-	assert!(numbers.len() == 2);
+	debug_assert!(numbers.len() == 2);
 	println!("{}", numbers[0] + numbers[1]);
 }
