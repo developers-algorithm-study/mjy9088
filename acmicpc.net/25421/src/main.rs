e33aca197a503654974f5a1e64e72aea1749611e
맹주영 <mjy9088@naver.com>
2022-08-17 23:44:53 +0900

    WIP: add 25421

---
+++ acmicpc.net/25421/src/main.rs
@@ -0,0 +1,27 @@
+use std::io;
+
+fn main() {
+    let mut line = String::new();
+    io::stdin().read_line(&mut line).unwrap();
+    let number = str::parse::<usize>(line.trim()).unwrap();
+    let result = (1..number).fold(vec![0, 1, 1, 1, 1, 1, 1, 1, 1, 1], |acc, _| {
+        (0..10)
+            .map(|x| {
+                // (0..10)
+                //     .map(|y| if x <= y + 2 && x + 2 >= y { acc[y] } else { 0 })
+                //     .for_each(|x| print!("{:5}", x));
+                // println!(
+                //     "{:6}",
+                //     (0..10)
+                //         .map(|y| if x <= y + 2 && x + 2 >= y { acc[y] } else { 0 })
+                //         .fold(0, |a, c| a + c)
+                // );
+                (0..10)
+                    .map(|y| if x <= y + 2 && x + 2 >= y { acc[y] } else { 0 })
+                    .fold(0, |a, c| (a + c) % 987654321)
+            })
+            .collect::<Vec<_>>()
+    });
+
+    println!("{}", result.iter().fold(0, |a, c| (a + c) % 987654321));
+}
