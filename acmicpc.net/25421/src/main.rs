83a1176830cfe63454d91e4a3f8ccc68d43b833b
맹주영 <mjy9088@naver.com>
2022-08-18 00:19:36 +0900

    solve 25421

--- acmicpc.net/25421/src/main.rs
+++ acmicpc.net/25421/src/main.rs
@@ -7,17 +7,14 @@ fn main() {
     let result = (1..number).fold(vec![0, 1, 1, 1, 1, 1, 1, 1, 1, 1], |acc, _| {
         (0..10)
             .map(|x| {
-                // (0..10)
-                //     .map(|y| if x <= y + 2 && x + 2 >= y { acc[y] } else { 0 })
-                //     .for_each(|x| print!("{:5}", x));
-                // println!(
-                //     "{:6}",
-                //     (0..10)
-                //         .map(|y| if x <= y + 2 && x + 2 >= y { acc[y] } else { 0 })
-                //         .fold(0, |a, c| a + c)
-                // );
                 (0..10)
-                    .map(|y| if x <= y + 2 && x + 2 >= y { acc[y] } else { 0 })
+                    .map(|y| {
+                        if x <= y + 2 && x + 2 >= y && x != 0 && y != 0 {
+                            acc[y]
+                        } else {
+                            0
+                        }
+                    })
                     .fold(0, |a, c| (a + c) % 987654321)
             })
             .collect::<Vec<_>>()
